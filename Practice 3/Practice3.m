bf5278cf1d1f06d1bfb8216c5825d10e62c6ac42
vuralerdogan <35148785+vuralerdogan@users.noreply.github.com>
2018-03-09 17:51:20 +0000

    Last adjustments

--- Practice 3/Practice3.m
+++ Practice 3/Practice3.m
@@ -1,7 +1,6 @@
-load dataOIL.mat
-%normalisation
-newDataTrn = 2*(DataTrn-min(DataTrn(:)))./(max(DataTrn(:))-min(DataTrn(:)))-1
-newDataTst = 2*(DataTst-min(DataTst(:)))./(max(DataTst(:))-min(DataTst(:)))-1
+load oil.mat
+newDataTrn = 2*(trndata-min(trndata(:)))./(max(trndata(:))-min(trndata(:)))-1
+newDataTst = 2*(tstdata-min(tstdata(:)))./(max(tstdata(:))-min(tstdata(:)))-1
 
 %checking data
 min(newDataTrn(:))
@@ -11,27 +10,21 @@ max(newDataTst(:))
 
 [pcvalues, pcvectors]= pca(newDataTrn);
 projdata = newDataTrn * pcvectors(:,1:2)
-[pcvalues2, pcvectors2]= pca(newDataTst);
-projdata2 = newDataTrn * pcvectors2(:,1:2)
+projdata2 = newDataTst * pcvectors(:,1:2)
+%ask the professor
 
-Label1=DataTrnLbls(:,1)
-Label2=DataTrnLbls(:,2)
-Label3=DataTrnLbls(:,3)
-
-
-Test1=DataTstLbls(:,1)
-Test2=DataTstLbls(:,2)
-Test3=DataTstLbls(:,3)
 figure(2)
 hold on;
-
 %Train Label and its projection
-plot(projdata(Label1==1,1),projdata(Label1==1,2),'b.')
-plot(projdata(Label2==1,1),projdata(Label2==1,2),'r.')
-plot(projdata(Label3==1,1),projdata(Label3==1,2),'c.')
+
+plot(projdata(trnlabels==1,1),projdata(trnlabels==1,2),'b.')
+plot(projdata(trnlabels==2,1),projdata(trnlabels==2,2),'r.')
+plot(projdata(trnlabels==3,1),projdata(trnlabels==3,2),'c.')
 
 %Test Label and its projection
-plot(projdata2(Test1==1,1), projdata2(Test1==1,2),'b*')
-plot(projdata2(Test2==1,1), projdata2(Test2==1,2),'r*')
-plot(projdata2(Test3==1,1), projdata2(Test3==1,2),'c*')
+plot(projdata2(tstlabels==1,1), projdata2(tstlabels==1,2),'b*')
+plot(projdata2(tstlabels==2,1), projdata2(tstlabels==2,2),'r*')
+plot(projdata2(tstlabels==3,1), projdata2(tstlabels==3,2),'c*')
+
+legend('Homogenus Trn', 'Analour Trn', 'Laminar Trn','Homogenus Tst', 'Analour Tst', 'Laminar Tst')
 savefig('Task 3')
