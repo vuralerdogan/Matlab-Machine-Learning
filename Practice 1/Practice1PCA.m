aac222d316d52142e8beecfd90b2c3192aa55c4f
vuralerdogan <35148785+vuralerdogan@users.noreply.github.com>
2018-03-14 04:45:32 +0000

    Last Practice, SVM with PCA, misclassification has been illustrated.

--- Practice 1/Practice1PCA.m
+++ Practice 1/Practice1PCA.m
@@ -18,14 +18,14 @@ figure(2)
 hold on;
 h1=plot(projdata(labels==1,1),projdata(labels==1,2),'r.');
 h2=plot(projdata(labels==2,1),projdata(labels==2,2),'b.');
-h1=plot(projdata(labels==3,1),projdata(labels==3,2),'g.');
+h3=plot(projdata(labels==3,1),projdata(labels==3,2),'g.');
 set(gca, 'Box','on');
 
 figure(5)
 hold on;
 h1=plot(projdata(labels==1,1),projdata(labels==1,2),'r.');
 h2=plot(projdata(labels==2,1),projdata(labels==2,2),'b.');
-h1=plot(projdata(labels==3,1),projdata(labels==3,2),'g.');
+h3=plot(projdata(labels==3,1),projdata(labels==3,2),'g.');
 set(gca, 'Box','on');
 
 run Practice1Kmeancls5.m
--- practical5/practical5.m
+++ practical5/practical5.m
@@ -1,5 +1,33 @@
 load data.mat
+load svmdata.mat
+%finding misclassifications
 compare=testingSet(:,1)
 result= predicted1 ~=compare
-k=find(result)
+order_misclass= find(result)
+%seperating data
+data=trainingset1(:,2:13)
+label=trainingset1(:,1)
+%normalisation and PCA
+normalisationdata= (data - mean(data).* ones(667,1))./std(data)
+mn=mean(normalisationdata)
+st=std(normalisationdata)
+[pcvals, pcvecs] = pca(normalisationdata)
+projdata = normalisationdata*pcvecs(:,1:2)
+
+figure(5)
+hold on;
+h1=plot(projdata(label==1,1),projdata(label==1,2),'r.');
+h2=plot(projdata(label==2,1),projdata(label==2,2),'b.');
+h3=plot(projdata(label==3,1),projdata(label==3,2),'g.');
+set(gca, 'Box','on');
+
+
+mis_class = normalisationdata(order_misclass,:)
+projclass = mis_class*pcvecs(:,1:2)
+h1=plot(projclass(:,1),projclass(:,2),'ko');
+legend('Class 1', 'Class 2', 'Class 3','Misclassification')
+
+
+
+
 
